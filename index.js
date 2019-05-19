d33e868d7dd1ebbfaf449536e664b504b21a51b3
Rectmoon <15692087320@163.com>
2019-05-19 17:15:46 +0800

    css

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
-app.use('*', (req, res, next) => {
-  res.end('404')
+app.listen(3000, () => {
+  console.log(`listening at port 3000`)
 })
-
-app.listen(3000)
--- readme.md
+++
@@ -1,1 +0,0 @@
-https://github.com/lky5230/tree-grid-by-vue
