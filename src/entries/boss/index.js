f1ce1d7c6a7c7483d09ddac1e7dbeced7cbb42a1
Rectmoon <15692087320@163.com>
2019-05-20 23:56:19 +0800

    boss 首页/列表页

--- src/entries/boss/index.js
+++ src/entries/boss/index.js
@@ -1,10 +1,13 @@
 if (process.env.NODE_ENV == 'development') require('raw-loader!./index.html')
 
+import '@/assets/styles/reset.styl'
+import './main.styl'
+
 console.log('这是boos项目')
 console.log(2)
 console.log(3)
 console.log(4)
 
-if (module.hot) {
-  module.hot.accept()
-}
+// if (module.hot) {
+//   module.hot.accept()
+// }
