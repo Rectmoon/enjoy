0913b04f985c7a53d7443ee443907080ef6ad939
Rectmoon <15692087320@163.com>
2019-06-03 00:57:32 +0800

    async await 支持

--- src/entries/index/index.js
+++ src/entries/index/index.js
@@ -1,4 +1,41 @@
 if (process.env.NODE_ENV == 'development') require('raw-loader!./index.html')
 
+require('es6-promise').polyfill()
+
 import '@/assets/styles/index.styl'
 import '@/assets/styles/layout.styl'
+
+function checkLogin() {
+  return true
+}
+
+function handleClick(e) {
+  if (!checkLogin()) {
+    console.log('请先登陆好吧？')
+    return
+  }
+  switch (e.target.id) {
+    case 'click':
+      console.log(111)
+      break
+    default:
+      break
+  }
+}
+
+document.addEventListener('click', handleClick, false)
+
+const sleep = t =>
+  new Promise(resolve =>
+    setTimeout(() => {
+      resolve(123)
+    }, t * 1000)
+  )
+
+async function login() {
+  let d = await sleep(2)
+
+  console.log(d)
+}
+
+login()
